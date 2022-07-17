5161532ea4f839796c31fcc0e04db40ae5407732
wicker250 <afonso.silveira1@hotmail.com>
2022-07-17 17:28:48 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import SalesCard from "./components/SalesCard";
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import Header from "./components/Header";
 function App() {
   return (
     <>
 
-
+      <ToastContainer/>
       <Header />
       <main>
         <section id="sales">
